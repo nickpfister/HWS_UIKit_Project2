e5c2c2de1dbbe96084afc07fd3f265eca710dfa3
Nick Pfister <7969388+nickpfister@users.noreply.github.com>
2021-11-29 22:29:56 -0800

    Completed challenge

--- HWS_UIKit_Project2/ViewController.swift
+++ HWS_UIKit_Project2/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    let maxGuesses = 10
+    
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
+    var totalAnswered = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +34,7 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        title = "[Score: \(score)] \(countries[correctAnswer].uppercased())"
         
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -43,18 +46,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        totalAnswered += 1
+        
         var title: String
+        var message: String
+        
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
+            message = "Good job!"
         } else {
             title = "Wrong"
             score -= 1
+            message = "You selected the flag for \(countries[sender.tag].uppercased())"
         }
             
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let handler = totalAnswered == maxGuesses ? gameOver : askQuestion
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: handler))
         present(ac, animated: true)
+    }
+    
+    func gameOver(action: UIAlertAction!) {
+        title = "Game Over!"
+        let message = "You guessed \(score)/\(maxGuesses) flags correctly."
+        let ac = UIAlertController(title: "Game Over!", message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: startOver))
+        present(ac, animated: true)
+    }
+    
+    func startOver(action: UIAlertAction!) {
+        score = 0
+        totalAnswered = 0
+        askQuestion()
     }
     
 }
